0d95c13de6c9f01a6194f27f517186988da8e2b1
Brian <pacebrian0@gmail.com>
2023-11-13 19:38:29 +0100

    bug

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AuthLayout = ({children}: React.ReactNode) => {
+const AuthLayout = ({children}: any) => {
   return (
     <div className="h-full flex items-center justify-center">
         {children}
