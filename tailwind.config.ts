3c4f01543c92f5abc79e70b75e61c2a78f65d5f7
Brian <pacebrian0@gmail.com>
2023-11-25 22:58:33 +0100

    Added file upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'tailwindcss'
+import { withUt } from "uploadthing/tw";
+ 
 
-const config: Config = {
+export default withUt({
   content: [
+    "./src/**/*.{ts,tsx,mdx}",
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,5 +19,5 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+});
+
